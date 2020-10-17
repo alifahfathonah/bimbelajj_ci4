1bcfbf544c143133c9aba5e78216dcfd8dd9bc93
mundir <mundinkcoy@gmail.com>
2020-10-18 00:08:31 +0700

    Add files via upload

---
+++ bimbelajj_db.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 17 Okt 2020 pada 15.37
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bimbelajj_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `class` text NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `batch` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
+(1, '2020-10-14-020023', 'App\\Database\\Migrations\\TblCalonPeserta', 'default', 'App', 1602655172, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_kelas`
+--
+
+CREATE TABLE `tbl_kelas` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nama_kelas` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_kelas`
+--
+
+INSERT INTO `tbl_kelas` (`id`, `nama_kelas`) VALUES
+(1, 'SD-Kelas 1'),
+(2, 'SD-Kelas 2'),
+(3, 'SMP-Kelas1'),
+(4, 'SMP-Kelas 2');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_peserta`
+--
+
+CREATE TABLE `tbl_peserta` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `nama` varchar(30) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `nomor_hp` varchar(12) DEFAULT NULL,
+  `asal_sekolah` varchar(255) DEFAULT NULL,
+  `kelas` int(2) UNSIGNED NOT NULL,
+  `alamat_sekolah` varchar(255) DEFAULT NULL,
+  `program_pilihan` int(2) UNSIGNED NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `tbl_peserta`
+--
+
+INSERT INTO `tbl_peserta` (`id`, `username`, `password`, `salt`, `nama`, `tgl_lahir`, `alamat`, `nomor_hp`, `asal_sekolah`, `kelas`, `alamat_sekolah`, `program_pilihan`, `created_at`, `updated_at`) VALUES
+(1, NULL, NULL, NULL, 'mundir muzaini', '2020-10-17', 'malang', '085700500836', 'malang', 1, 'malang jawa timur', 1, '2020-10-17 02:21:51', '2020-10-17 02:21:51'),
+(2, NULL, NULL, NULL, 'mundir muzaini', '1111-10-17', 'malang indonesisa', '085700500836', 'malang', 1, 'malang jawa timur', 5, '2020-10-17 02:24:26', '2020-10-17 02:24:26');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_program_pilihan`
+--
+
+CREATE TABLE `tbl_program_pilihan` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `kode_program` varchar(10) NOT NULL,
+  `nama_program` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_program_pilihan`
+--
+
+INSERT INTO `tbl_program_pilihan` (`id`, `kode_program`, `nama_program`) VALUES
+(1, 'bimreg', 'Bimbingan Reguler SD, SMP, SMA'),
+(2, 'bimint', 'Bimbingan Intensif PTN'),
+(3, 'klnkbel', 'Klinik Belajar'),
+(4, 'kursrob', 'Kursus Robotik'),
+(5, 'bmkprak', 'BMK Praktis'),
+(6, 'smrtarb', 'Smart Arabic');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_tutor`
+--
+
+CREATE TABLE `tbl_tutor` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `modified_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_kelas`
+--
+ALTER TABLE `tbl_kelas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_peserta`
+--
+ALTER TABLE `tbl_peserta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_program_pilihan`
+--
+ALTER TABLE `tbl_program_pilihan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_tutor`
+--
+ALTER TABLE `tbl_tutor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_kelas`
+--
+ALTER TABLE `tbl_kelas`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_peserta`
+--
+ALTER TABLE `tbl_peserta`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_program_pilihan`
+--
+ALTER TABLE `tbl_program_pilihan`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_tutor`
+--
+ALTER TABLE `tbl_tutor`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
