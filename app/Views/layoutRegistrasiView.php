ae497529a630e1dbfb5efb7265b0e96c10ccb9fc
mundir muzaini <mundinkcoy@gmail.com>
2020-10-18 23:12:27 +0700

    layouting

---
+++ app/Views/layoutRegistrasiView.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Bimbelajj - Form Registrasi Peserta Baru</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta content="Bimbelajj" name="description" />
+    <meta content="Mundir Muzaini" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="/template/assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="/template/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="/template/assets/css/icons.css" rel="stylesheet" type="text/css" />
+    <link href="/template/assets/css/metismenu.min.css" rel="stylesheet" type="text/css" />
+    <link href="/template/assets/css/style.css" rel="stylesheet" type="text/css" />
+
+    <script src="/template/assets/js/modernizr.min.js"></script>
+
+</head>
+
+
+<body class="account-pages">
+
+    <!-- Begin page -->
+    <div class="accountbg" style="background: url('/template/assets/images/bg-2.jpg');background-size: cover;"></div>
+
+    <div class="wrapper-page account-page-full">
+
+        <!-- disini tempat isinya -->
+        <?php echo $this->renderSection('content'); ?>
+
+
+    </div>
+
+
+
+    <!-- jQuery  -->
+    <script src="/template/assets/js/jquery.min.js"></script>
+    <script src="/template/assets/js/popper.min.js"></script>
+    <script src="/template/assets/js/bootstrap.min.js"></script>
+    <script src="/template/assets/js/metisMenu.min.js"></script>
+    <script src="/template/assets/js/waves.js"></script>
+    <script src="/template/assets/js/jquery.slimscroll.js"></script>
+
+    <!-- App js -->
+    <script src="/template/assets/js/jquery.core.js"></script>
+    <script src="/template/assets/js/jquery.app.js"></script>
+
+</body>
+
+</html>
---
+++ app/Views/registrasi/loginView.php
@@ -0,0 +1,101 @@
+<?= $this->extend('layoutRegistrasiView') ?>
+<?= $this->section('content') ?>
+
+
+<div class="card">
+    <div class="card-block">
+
+        <div class="account-box">
+
+            <div class="card-box p-5">
+                <h2 class="text-uppercase text-center pb-0">
+                    <a href="index.html" class="text-success">
+                        <span><img src="/template/assets/images/logo.png" alt="" height="26"></span>
+                    </a>
+                </h2>
+                <div class="row pb-4">
+                    <div class="col-sm-12 text-center">
+                        <p class="text-muted">Masuk ke akun anda di Bimbelajj </p>
+                        <hr>
+                    </div>
+                </div>
+                <?php
+                echo $pesanError;
+
+                echo form_open('registrasi/login_procs', 'class="form-horizontal"');
+                $fld = 'username';
+                $class = ($validasi->hasError($fld)) ? 'form-control is-invalid' : 'form-control';
+                $extra = [
+                    'id' => $fld,
+                    'class' => $class,
+                    'required' => 'required'
+                ];
+                $val = (old($fld) != null) ? old($fld) : '';
+                $session = session();
+                $flashdata = $session->getFlashdata('username');
+                if ($flashdata != null) {
+                    $val = $flashdata;
+                }
+                $err = $validasi->getError($fld);
+                ?>
+
+                <div class="form-group row m-b-20">
+                    <div class="col-12">
+                        <?= form_label('Username', $fld); ?>
+                        <?= form_input($fld, $val, $extra); ?>
+                        <div class="invalid-feedback">
+                            <?= $err ?>
+                        </div>
+                    </div>
+                </div>
+
+
+                <?php
+                $fld = 'password';
+                $class = ($validasi->hasError($fld)) ? 'form-control is-invalid' : 'form-control';
+                $extra = [
+                    'id' => $fld,
+                    'class' => $class,
+                    'required' => 'required'
+                ];
+                $val = (old($fld) != null) ? old($fld) : '';
+                $err = $validasi->getError($fld);
+                ?>
+                <div class="form-group row m-b-20">
+                    <div class="col-12">
+                        <?= form_label('Password, password awal adalah 123', $fld); ?>
+                        <?= form_password($fld, '', $extra); ?>
+                        <div class="invalid-feedback">
+                            <?= $err ?>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="form-group row text-center m-t-10">
+                    <div class="col-12">
+                        <button class="btn btn-block btn-custom waves-effect waves-light" type="submit">Login</button>
+                    </div>
+                </div>
+
+                <?= form_close(); ?>
+
+                <div class="row m-t-50">
+                    <div class="col-sm-12 text-center">
+                        <hr>
+                        <p class="text-muted">Belum punya akun? <a href="/registrasi" class="text-dark m-l-5"><b>Registrasi</b></a></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <div class="m-t-4 text-center">
+        <p class="account-copyright">2020 © Bimbelajj.com</p>
+    </div>
+</div>
+
+
+<?= $this->endSection() ?>
